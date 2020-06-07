006a879888f24f47a473a35b21c5377df8a52910
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-07 19:49:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,11 +2,8 @@
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
-  "Hello #{player1} & #{playar2}"
-  puts start_game("Rocky", "Bullwinkle")
 end
 
 def play_game(player2, player1)
   "#{player1} is better than #{player2}"
-  puts play_game("Jay-Z", "Beyonce")
 end
