cafe90df24a3e3de561d885ce070c03823d82b7b
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-07 19:48:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,8 +2,11 @@
 
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{playar2}"
+  puts start_game("Rocky", "Bullwinkle")
 end
 
-def play_game(player2)
+def play_game(player2, player1)
   "#{player1} is better than #{player2}"
+  puts play_game("Jay-Z", "Beyonce")
 end
